618b3bb6a040eebfe0d773d3e50f4fce6d39a377
Yacine Mekesser <yacine.mekesser@zuehlke.com>
2021-11-11 15:57:14 +0800

    Add Tasklist component

---
+++ src/components/TaskList.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Task from './Task';
+
+export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+    const events = {
+        onPinTask,
+        onArchiveTask,
+    };
+
+    if (loading) {
+        return LoadingPlaceholder;
+    }
+
+    if (tasks.length === 0) {
+        return EmptyPlaceholder;
+    }
+
+    const sortedTasks = sortTasks(tasks);
+
+    return (<div className="list-items">
+        {sortedTasks.map(t => (
+            <Task key={t.id} task={t} {...events}></Task>
+        ))}
+    </div>
+    );
+}
+
+function sortTasks(tasks) {
+    return [
+        ...tasks.filter(t => t.state === 'TASK_PINNED'),
+        ...tasks.filter(t => t.state !== 'TASK_PINNED'),
+    ]
+}
+
+const LoadingRow = (
+    <div className="loading-item">
+        <span className="glow-checkbox"></span>
+        <span className="glow-text">
+            <span> Loading </span>
+            <span> cool </span>
+            <span> state </span>
+        </span>
+    </div>
+)
+
+const LoadingPlaceholder = (
+    <div className="list-items">
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+    </div>
+)
+
+const EmptyPlaceholder = (
+    <div className="list-items">
+        <div className="wrapper-message">
+            <span className="icon-check"></span>
+            <div className="title-message">You have no tasks</div>
+            <div className="subtitle-message">Sit back and relax</div>
+        </div>
+    </div>
+)
+
+TaskList.propTypes = {
+ /** Checks if it's in loading state */
+ loading: PropTypes.bool,
+ /** The list of tasks */
+ tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+ /** Event to change the task to pinned */
+ onPinTask: PropTypes.func,
+ /** Event to change the task to archived */
+ onArchiveTask: PropTypes.func,
+};
+TaskList.defaultProps = {
+ loading: false,
+};
